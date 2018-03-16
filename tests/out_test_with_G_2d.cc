2c85ff2943bd9fbbe37f42f152eae28fe91611f7
Nicola Giuliani <ngiuliani@sissa.it>
2018-03-16 13:09:05 +0100

    hopefully working

--- tests/out_test_with_G_2d.cc
+++ tests/out_test_with_G_2d.cc
@@ -130,7 +130,8 @@ int main (int argc, char **argv)
       post_process.fe_stokes = std::unique_ptr<FiniteElement<dim-1, dim> >(new FESystem<dim-1, dim>(FE_Q<dim-1,dim> (degree),dim));
       post_process.fe_map = std::unique_ptr<FiniteElement<dim-1, dim> >(new FESystem<dim-1, dim>(FE_Q<dim-1,dim> (degree),dim));
       post_process.grid_fe = std::unique_ptr<FiniteElement<2, dim> >(new FESystem<2, dim>(FE_Q<2,dim> (degree),dim));
-
+      post_process.reflect_kernel = false;
+      post_process.no_slip_kernel = false;
       SphericalManifold<dim-1,dim> manifold;
       post_process.tria.set_all_manifold_ids(0);
       post_process.tria.set_manifold(0, manifold);
